006b88c2eef2874ab9f97036b0e317776337b0f5
beckireby15@gmail.com <beckireby15@gmail.com>
2022-08-22 11:25:00 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array, name) {
+    let messages = [];
+    for (let i = 0; i < array.length; i++) {
+      messages.push(`Thank you, ${array[i]}, for the wonderful ${name} gift!`);
+    }
+    // console.log(messages)
+    return messages;
+  }
+  writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+  
+  // Second Section
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  countDown(4);
